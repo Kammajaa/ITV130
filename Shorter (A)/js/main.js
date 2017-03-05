ee6602a6b36cdef08ae9d5936353eec715b84b27
Mart <marrrt1@gmail.com>
2017-03-05 16:18:50 +0200

    css fix

--- Shorter (A)/js/main.js
+++ Shorter (A)/js/main.js
@@ -15,10 +15,12 @@ $(function() {
         updateCheckbox($(this));
     });
 
-    $("body").on("click", ".sub-form", function() {
-        if (!$(this).hasClass("display")) {
-            $(this).closest(".container").find(".sub-form").removeClass("display");
-            $(this).addClass("display");
+    $("body").on("click", ".sub-form h4", function() {
+        if (!$(this).parent().hasClass("display")) {
+            $(this).parent().closest(".container").find(".sub-form").removeClass("display");
+            $(this).parent().addClass("display");
+        } else {
+            $(this).parent().removeClass("display");
         }
     });
 });
