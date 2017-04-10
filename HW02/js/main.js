17a2b0300a7474d94cd18bc2ee71505fdb8f4cd3
Kristjan Düüna <kristjan1301@gmail.com>
2017-04-10 23:57:44 +0300

    shuffle

--- HW02/js/main.js
+++ HW02/js/main.js
@@ -1,9 +1,39 @@
+var m1, m2, m3;
+
 $(function() {
+
+
+    m1 = $('#slot1').slotMachine({
+        active: 4,
+        delay: 1000
+    });
+
+    m2 = $('#slot2').slotMachine({
+        active: 4,
+        delay: 1000
+    });
+
+    m3 = $('#slot3').slotMachine({
+        active: 4,
+        delay: 1000
+    });
+
+
+
+    $('body').on('click', '.nob', function() {
+        m1.shuffle(1);
+        m2.shuffle(1);
+        m3.shuffle(1);
+    });
+
+
     $(".jou").click(function() {
         $("#winSound").play();
         $(".jou").hide()
-    })
-})
+    });
+
+});
+
 
 function playWinSound() {
     document.getElementById("winSound").play();
