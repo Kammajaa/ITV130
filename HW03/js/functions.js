127824cf42168e94f81fafae5e261a83d9cf1b4d
Mart <mart.hytt@gmail.com>
2017-05-01 12:59:25 +0300

    first view

--- HW03/js/functions.js
+++ HW03/js/functions.js
@@ -1,3 +1,9 @@
 $(function() {
+    $("#input-username").focusin(function() {
+        $(this).parent().addClass("active");
+    });
 
+    $("#input-username").focusout(function() {
+        $(this).parent().removeClass("active");
+    });
 });
